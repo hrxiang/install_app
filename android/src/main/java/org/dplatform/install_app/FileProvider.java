81baac71b034f4541894d1b81659b16bb0e5e19a
xianghairui <xianghairui@up-tech.cn>
2020-04-03 17:52:03 +0800

    init

--- android/src/main/java/org/dplatform/install_app/FileProvider.java
+++ android/src/main/java/org/dplatform/install_app/FileProvider.java
@@ -8,10 +8,9 @@ import java.io.File;
 
 
 public class FileProvider extends androidx.core.content.FileProvider {
-    private static final String FILE_PROVIDER_AUTHORITIES_VALUE = "%s.fileProvider";
 
     public static Uri getUriForFile(Activity activity, File file) {
-        String author = String.format(FILE_PROVIDER_AUTHORITIES_VALUE, activity.getPackageName());
+        String author = String.format("%s.dplatform.fileProvider", activity.getPackageName());
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return androidx.core.content.FileProvider.getUriForFile(activity, author, file);
         } else {
